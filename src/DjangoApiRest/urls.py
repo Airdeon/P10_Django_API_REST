6fff938f2a7396b0f869ce04ae7d2b4d17f6c564
Airdeon <airdeon@gmail.com>
2022-08-10 15:07:02 +0200

    debut de router nested

--- src/DjangoApiRest/urls.py
+++ src/DjangoApiRest/urls.py
@@ -19,19 +19,24 @@ from rest_framework import routers
 from API.views import ProjectsViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from Accounts.views import Signup, UserViewSet
+from rest_framework_nested import routers
 
 # API URL
 router = routers.SimpleRouter()
 
 # router.register("signup", ViewSetSignup, basename="signup")
-router.register("projects", ProjectsViewSet, basename="projects")
-# router.register("projects/pk/users/", UserViewSet, basename="projects_user")
+router.register(r"projects", ProjectsViewSet, basename="projects")
+# router.register("projects/<int:project_id>/users/", UserViewSet, basename="projects_user")
+
+domains_router = routers.NestedSimpleRouter(router, r"projects", lookup="projects")
+domains_router.register(r"users", UserViewSet, basename="domain-nameservers")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/", include(router.urls)),
+    path(r"", include(router.urls)),
+    path(r"", include(domains_router.urls)),
     path("signup/", Signup.as_view(), name="signup"),
 ]
